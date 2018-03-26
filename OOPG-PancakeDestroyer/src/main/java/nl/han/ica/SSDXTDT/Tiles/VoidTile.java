ec6f19fab7beaad0889f109e90401758917903ea
jeroenKempen <jvankempen@hotmail.nl>
2018-03-26 16:05:06 +0200

    alles

---
+++ OOPG-PancakeDestroyer/src/main/java/nl/han/ica/SSDXTDT/Tiles/VoidTile.java
@@ -0,0 +1,15 @@
+package nl.han.ica.SSDXTDT.Tiles;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
+import nl.han.ica.OOPDProcessingEngineHAN.Tile.Tile;
+
+public class VoidTile extends Tile{
+
+
+    /**
+     * @param sprite The image which will be drawn whenever the draw method of the Tile is called.
+     */
+    public VoidTile(Sprite sprite) {
+        super(sprite);
+    }
+}
