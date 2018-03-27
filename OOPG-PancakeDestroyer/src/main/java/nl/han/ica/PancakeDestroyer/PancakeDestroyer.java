8648645b1b471d17519937e564df3c27641d0754
jeroenKempen <jvankempen@hotmail.nl>
2018-03-27 10:25:00 +0200

    ons spel staat er nu in

---
+++ OOPG-PancakeDestroyer/src/main/java/nl/han/ica/PancakeDestroyer/PancakeDestroyer.java
@@ -0,0 +1,21 @@
+package nl.han.ica.PancakeDestroyer;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Engine.GameEngine;
+import processing.core.PApplet;
+
+public class PancakeDestroyer extends GameEngine{
+
+    public static void main(String[] args) {
+        PApplet.main(new String[]{"nl.han.ica.PancakeDestroyer.PancakeDestroyer"});
+    }
+
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
