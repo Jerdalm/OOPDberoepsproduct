6a9a557c8fc4bbe3611477740cb68e1fa340f59d
jeroenKempen <jvankempen@hotmail.nl>
2018-03-28 13:06:06 +0200

    main

--- OOPG-PancakeDestroyer/src/main/java/nl/han/ica/PancakeDestroyer/PancakeDestroyer.java
+++ OOPG-PancakeDestroyer/src/main/java/nl/han/ica/PancakeDestroyer/PancakeDestroyer.java
@@ -6,10 +6,12 @@ import nl.han.ica.OOPDProcessingEngineHAN.View.View;
 import nl.han.ica.waterworld.TextObject;
 import processing.core.PApplet;
 
+import java.util.ArrayList;
+
 public class PancakeDestroyer extends GameEngine{
 
-    private TextObject dashboardText;
-    private int test = 0;
+    private ArrayList<TextObject> dashboardText = new ArrayList<TextObject>();
+    private int bricks = 8;
 
     public static void main(String[] args) {
         PApplet.main(new String[]{"nl.han.ica.PancakeDestroyer.PancakeDestroyer"});
@@ -24,21 +26,31 @@ public class PancakeDestroyer extends GameEngine{
         setView(view);
         size(worldWidth, worldHeight);
 
-        createDashboard(worldWidth, 200);
+        createDashboard(worldWidth, 150, worldWidth / 2, worldHeight-150);
 
     }
 
     @Override
     public void update() {
-        test ++;
-        dashboardText.setText("Bubbles popped: " + test);
+            dashboardText.get(0).setText("Bricks: " + bricks);
+            dashboardText.get(1).setText("hoooooiiii");
+            dashboardText.get(2).setText("hoi jeremy");
     }
 
-    private void createDashboard(int dashboardWidth,int dashboardHeight) {
-        Dashboard dashboard = new Dashboard(0,0, dashboardWidth, dashboardHeight);
+    public void mousePressed() {
+        bricks--;
+    }
+
+    private void createDashboard(int dashboardWidth,int dashboardHeight, int x, int y) {
+        Dashboard dashboard = new Dashboard(x, y, dashboardWidth, dashboardHeight);
         dashboard.setBackground(138,144,150);
-        dashboardText=new TextObject("");
-        dashboard.addGameObject(dashboardText);
+        for (int i = 0; i < 3; i ++) {
+            int yNew= - i * 50 + y;
+            dashboardText.add(new TextObject(""));
+            dashboardText.get(i) .setX(-x);
+            dashboardText.get(i) .setY(-yNew);
+            dashboard.addGameObject(dashboardText.get(i));
+        }
         addDashboard(dashboard);
     }
 
