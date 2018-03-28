04721cd81b15af9b0ce46a6c07994238032d60de
jeroenKempen <jvankempen@hotmail.nl>
2018-03-28 11:00:32 +0200

    dashbord

--- OOPG-PancakeDestroyer/src/main/java/nl/han/ica/PancakeDestroyer/PancakeDestroyer.java
+++ OOPG-PancakeDestroyer/src/main/java/nl/han/ica/PancakeDestroyer/PancakeDestroyer.java
@@ -9,6 +9,7 @@ import processing.core.PApplet;
 public class PancakeDestroyer extends GameEngine{
 
     private TextObject dashboardText;
+    private int test = 0;
 
     public static void main(String[] args) {
         PApplet.main(new String[]{"nl.han.ica.PancakeDestroyer.PancakeDestroyer"});
@@ -23,11 +24,23 @@ public class PancakeDestroyer extends GameEngine{
         setView(view);
         size(worldWidth, worldHeight);
 
+        createDashboard(worldWidth, 200);
+
     }
 
     @Override
     public void update() {
-
+        test ++;
+        dashboardText.setText("Bubbles popped: " + test);
     }
+
+    private void createDashboard(int dashboardWidth,int dashboardHeight) {
+        Dashboard dashboard = new Dashboard(0,0, dashboardWidth, dashboardHeight);
+        dashboard.setBackground(138,144,150);
+        dashboardText=new TextObject("");
+        dashboard.addGameObject(dashboardText);
+        addDashboard(dashboard);
+    }
+
 
 }
