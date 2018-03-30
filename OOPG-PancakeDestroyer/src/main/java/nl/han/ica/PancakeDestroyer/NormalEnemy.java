f754bea191993181a0f96a9127f66df797fffa6b
Jerdalm <36266491+Jerdalm@users.noreply.github.com>
2018-03-30 15:30:25 +0200

    random

--- OOPG-PancakeDestroyer/src/main/java/nl/han/ica/PancakeDestroyer/NormalEnemy.java
+++ OOPG-PancakeDestroyer/src/main/java/nl/han/ica/PancakeDestroyer/NormalEnemy.java
@@ -3,46 +3,63 @@ package nl.han.ica.PancakeDestroyer;
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.SpriteObject;
 
+import javax.naming.ldap.Rdn;
 import java.util.Random;
 
 
 public class NormalEnemy extends Pancake {
 
-    private Random r = new Random();
-    private Random t = new Random();
-    private Random Rdirection = new Random();
-    private int randomNumber;
+    private Random yStartpoint = new Random();//random plaatsing op de Y as
+    private Random Xstartpoint = new Random();//random plaatsing op de X as
+    private Random Rdirection = new Random();//random richting van de pannekoek
+    private float dir;
+    private int randomYcod;
+    private int randomXcod;
     private float Ycod;
+    private float Xcod;
+    private boolean turningPoint = false;
 
 
     public NormalEnemy(PancakeDestroyer world) {
         super(new Sprite("src/main/java/nl/han/ica/PancakeDestroyer/media/EvilPancake.png"), world);
-        randomNumber = r.nextInt(800);
-        Ycod = randomNumber;
-        randomNumber = r.nextInt(0);
-        setxSpeed(5);
+        randomYcod = yStartpoint.nextInt(680);//zet de random om naar een int
+        Ycod = randomYcod;//zet de int om naar een float
+
+        randomXcod = Xstartpoint.nextInt(200);//zet de random om naar een int
+        Xcod = randomXcod;//zet de int om naar een float
+
+        dir = Rdirection.nextInt(360);//zet de random om naar een int
+        setxSpeed(6);
         setY(Ycod);
-        if(r.equals(0)) {
+
+        if (Xcod < 100) {
             setX(-200);
             setDirection(90);
-        }else{
-            setX(world.getWidth()+200);
+        } else {
+            setX(world.getWidth() + 200);
             setDirection(270);
         }
     }
 
-    public void getHit(){
-        
+    public void getHit() {
+
     }
 
     @Override
     public void update() {
-        System.out.println(Ycod);
 
-        if(r.equals(0) && getX() > 800){
-            setDirection(Rdirection);
+        if (Xcod < 100 && getX() >=900 && !turningPoint) {
+            setDirection(dir);
+            turningPoint = true;
+            System.out.println("biem: "+ Xcod);
+            System.out.println(Ycod);
         }
-
+        if(Xcod > 100 && getX() <=300 && !turningPoint) {
+            setDirection(dir);
+            turningPoint = true;
+            System.out.println("biem: "+ Xcod);
+            System.out.println(Ycod);
+        }
 //        }
 //        if (getX() > 800 && getY() + getHeight() < 550){
 //            setDirection(225);
@@ -50,5 +67,6 @@ public class NormalEnemy extends Pancake {
 //        if (getX()+ getWidth() < 500) {
 //            setDirection(91);
 //        }
+        }
     }
-}
+
