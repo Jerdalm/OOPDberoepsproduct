9c8c60d0473e38b900e310932b26fe1443bbef4c
jeroenKempen <jvankempen@hotmail.nl>
2018-03-30 13:37:57 +0200

    hoi

--- OOPG-PancakeDestroyer/src/main/java/nl/han/ica/PancakeDestroyer/NormalEnemy.java
+++ OOPG-PancakeDestroyer/src/main/java/nl/han/ica/PancakeDestroyer/NormalEnemy.java
@@ -8,7 +8,6 @@ import java.util.Random;
 
 public class NormalEnemy extends Pancake {
 
-    private PancakeDestroyer world;
     private Random r = new Random();
     private int randomNumber;
     private float Ycod;
@@ -16,7 +15,6 @@ public class NormalEnemy extends Pancake {
 
     public NormalEnemy(PancakeDestroyer world) {
         super(new Sprite("src/main/java/nl/han/ica/PancakeDestroyer/media/EvilPancake.png"), world);
-        this.world=world;
         randomNumber = r.nextInt(1);
         Ycod = randomNumber*800;
         r.nextInt(1);
