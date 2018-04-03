ee8135ac7b5b1b92fc2f831e181eaa9ac619dfd0
Jerdalm <36266491+Jerdalm@users.noreply.github.com>
2018-04-03 11:04:14 +0200

    oi m8

--- OOPG-PancakeDestroyer/src/main/java/nl/han/ica/PancakeDestroyer/Pancakes/UltimateEnemyPancake.java
+++ OOPG-PancakeDestroyer/src/main/java/nl/han/ica/PancakeDestroyer/Pancakes/UltimateEnemyPancake.java
@@ -20,8 +20,9 @@ public class UltimateEnemyPancake extends Pancake {
         public void update () {
             if (hit == false) {
                 if (Xcod < world.getWorldWidth() / 2 - 100) {//maak een keerpunt op 70 procent van de afgelegde weg
-                    if (getX() <= getWidth() / 2 + 200 && getX() >= getWidth() / 2){
+                    if (getX() <= getWidth() / 2 + 200 && getX() >= getWidth() / 2 && !turningPoint){
                         setDirection(45);
+                        turningPoint = true;
                     }
                     if (getX() >= getWidth() / 2 +200){
                         setDirection(300);
@@ -29,10 +30,11 @@ public class UltimateEnemyPancake extends Pancake {
                         setDirection(270);
                     }
                 } else {//maak een keerpunt op 70 procent van de afgelegde weg
-                    if (getX() <= getWidth() / 2 - 200 && getX() >= getWidth() / 2) {
+                    if (getX() >= getWidth() / 2 - 200 && getX() <= getWidth() / 2 && !turningPoint) {
                         setDirection(45);
+                        turningPoint = true;
                     }
-                    if (getX() >= 700) {
+                    if (getX() >= getWidth() / 2 + 200) {
                         setDirection(300);
                     } else {
                         setDirection(270);
