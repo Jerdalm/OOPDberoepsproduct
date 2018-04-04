879ee411330d363d5343ba0231fafca5d1d044d1
Jerdalm <36266491+Jerdalm@users.noreply.github.com>
2018-04-04 14:23:05 +0200

    HAHHAHAHAHAHAHAHAHAHAAHAHAHAHAHAHAHAHAHAHAHAAHAHAHAHAHAHAHAHA

--- OOPG-PancakeDestroyer/src/main/java/nl/han/ica/PancakeDestroyer/Pancakes/ConfusedPancake.java
+++ OOPG-PancakeDestroyer/src/main/java/nl/han/ica/PancakeDestroyer/Pancakes/ConfusedPancake.java
@@ -11,7 +11,9 @@ public class ConfusedPancake extends Pancake {
 
     private boolean down;
     private boolean left;
-    private int zigzagDistance = 50;
+    private boolean up;
+    private boolean right;
+    private int zigzagDistance = 60;
 
     /**
      * @param world is de wereld waarin de NormalEnemy Spawned
@@ -19,31 +21,35 @@ public class ConfusedPancake extends Pancake {
     public ConfusedPancake(PancakeDestroyer world) {
         super(new Sprite("src/main/java/nl/han/ica/PancakeDestroyer/media/ConfusedPancake.png"), world);
         points = 5;
-        setxSpeed(8);
+        setxSpeed(10);
 
         if (dir < 90) {//Pannekoek komt van rechts
             setX(-getWidth());
             setY(Ycod);
             setDirection(35);
-            left = false;
+            left = true;
+            System.out.println("a");
         }
         if (dir > 90 && dir < 180) {//Pannekoek komt van onder
             setX(Xcod);
             setY(-getHeight());
             setDirection(125);
             down = true;
+            System.out.println("b");
         }
         if (dir > 180 && dir < 270) {//Pannekoek komt van links
             setX(world.getWidth() + getWidth());
             setY(Ycod);
             setDirection(215);
-            left = true;
+            right = true;
+            System.out.println("c");
         }
         if (dir > 270) {//Pannekoek komt van boven
             setX(Xcod);
             setY(world.getHeight() + getHeight());
             setDirection(305);
-            down = false;
+            up = true;
+            System.out.println("d");
         }
     }
 
@@ -51,13 +57,15 @@ public class ConfusedPancake extends Pancake {
     public void update() {
         if (hit == false) {
             if (down) {
+                System.out.println("1");
                 if (getX() <= Xcod - zigzagDistance) {
                     setDirection(125);
                 }
                 if (getX() >= Xcod + zigzagDistance) {
                     setDirection(225);
                 }
-            } if(!down) {
+            } if(up) {
+                System.out.println("2");
                 if (getX() <= Xcod - zigzagDistance) {
                     setDirection(35);
                 }
@@ -65,14 +73,16 @@ public class ConfusedPancake extends Pancake {
                     setDirection(305);
                 }
             }
-            if (left) {
+            if (right) {
+                System.out.println("3");
                 if (getY() <= Ycod - zigzagDistance) {
-                    setDirection(305);
-                }
-                if (getY() >= Ycod + zigzagDistance) {
                     setDirection(215);
                 }
-            } if (!left){
+                if (getY() >= Ycod + zigzagDistance) {
+                    setDirection(305);
+                }
+            } if (left){
+                System.out.println("4");
                 if (getY() <= Ycod - zigzagDistance) {
                     setDirection(125);
                 }
