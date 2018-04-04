309c4fc10011740943eb05ab6b908e32cc8bb104
jeroenKempen <jvankempen@hotmail.nl>
2018-04-04 12:10:52 +0200

    new class

---
+++ OOPG-PancakeDestroyer/src/main/java/nl/han/ica/PancakeDestroyer/PancakeSpawner.java
@@ -0,0 +1,61 @@
+package nl.han.ica.PancakeDestroyer;
+
+import nl.han.ica.PancakeDestroyer.Pancakes.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PancakeSpawner {
+
+    PancakeDestroyer world;
+    private ArrayList<Pancake> pancakes = new ArrayList<>();
+
+    public PancakeSpawner(PancakeDestroyer world) {
+        this.world = world;
+    }
+
+    public void update() {
+        spawnPancakes();
+        deletePancakes();
+    }
+
+    private void spawnPancakes() {
+        Random random = new Random();
+        int type = random.nextInt(4);
+        if (pancakes.size() < 5) {
+            if (type == 0) {
+                Pancake pancake = new FriendlyPancake(world);
+                pancakes.add(pancake);
+                world.addGameObject(pancake, 1);
+            } else if (type == 1) {
+                Pancake pancake = new NormalEnemy(world);
+                pancakes.add(pancake);
+                world.addGameObject(pancake, 1);
+            } else if (type == 2) {
+                Pancake pancake = new FastEnemyPancake(world);
+                pancakes.add(pancake);
+                world.addGameObject(pancake, 1);
+            } else if (type == 3) {
+                Pancake pancake = new UltimateEnemyPancake(world);
+                pancakes.add(pancake);
+                world.addGameObject(pancake);
+            }
+        }
+    }
+
+
+    private void deletePancakes() {
+        for (int i = 0; i < pancakes.size(); i++) {
+            if (pancakes.get(i).getX() > world.getWorldWidth() + 200 || pancakes.get(i).getX() < -200 ||
+                    pancakes.get(i).getY() > world.getWorldHeight() + 200 || pancakes.get(i).getY() < -200) {
+                world.deleteGameObject(pancakes.get(i));
+                pancakes.remove(i);
+                i --;
+            }
+        }
+    }
+
+    public ArrayList<Pancake> getPancakes() {
+        return pancakes;
+    }
+}
+
